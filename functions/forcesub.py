76cfb23562dbb26d7b482f888cd88d8f8f2ffa78
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-03-01 22:20:39 +0530

    Update forcesub.py

--- functions/forcesub.py
+++ functions/forcesub.py
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+            text="Something Went Wrong. Contact My [OWNER](https://t.me/TEAM_SNS_BOT).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
