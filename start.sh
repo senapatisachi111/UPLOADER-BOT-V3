0a7c5efc8880a8c1f2cc61884aa5059e99ca375a
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-03-01 21:38:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
+git clone https://github.com/senapatisachi111/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
 cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
 echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
